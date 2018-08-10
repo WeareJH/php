eea053aed2ea9901da2774c50c08bcbdf1e41087
Aneurin Barker Snook <a@aneur.in>
2018-08-10 12:19:27 +0100

    jail commands to www-data, allow build w cache
    1) to try to improve file permissions management, we can run the container as root if starting php-fpm or bash, but otherwise sequester to www-data e.g. composer, yarn?
    
    2) no benefit to locally building without cache, replace in build script with arg2. can pass --no-cache directly to the call, as done in circleci config

--- build.sh
+++ build.sh
@@ -16,4 +16,4 @@ fi
 docker pull ${REPO}:${PHPVER}
 docker tag ${REPO}:${PHPVER} ${REPO}:${PHPVER}-current
 
-docker build --no-cache -f ${PHPVER}/Dockerfile -t ${REPO}:${PHPVER} ${HERE}
+docker build ${2} -f ${PHPVER}/Dockerfile -t ${REPO}:${PHPVER} ${HERE}
